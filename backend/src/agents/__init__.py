2faa896b238dcfae155db5fe96fc160f476ebd0d
zyx <814780329@qq.com>
2026-05-31 09:58:16 +0800

    reporter实现

--- backend/src/agents/__init__.py
+++ backend/src/agents/__init__.py
@@ -1,9 +1,11 @@
 from .recoder import ToolAwareSimpleAgent
 from .planner import Planner
 from .summarizer import Summarizer
+from .reporter import Reporter
 
 __all__ = [
     "ToolAwareSimpleAgent",
     "Planner",
-    "Summarizer"
+    "Summarizer",
+    "Reporter"
 ]
